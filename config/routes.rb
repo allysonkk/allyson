4afb76959b19fccaf6f1bb10fc5f079857e20fc1
Felipe Oliveira <felipe.oliveira@abril.com.br>
2012-09-16 18:39:55 -0300

    corrindo routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Allyson::Application.routes.draw do
   # get "home/index"
-  # root :to => "home#index"
+  root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +50,7 @@ Allyson::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'welcome#index'
+  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
