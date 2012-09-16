5faca75b25e040594c76e9195bd39f25a5b6f8cf
root <root@ip-10-252-29-38.sa-east-1.compute.internal>
2012-09-16 19:54:27 +0000

    iniciando aplicação

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Allyson::Application.config.secret_token = '06e8b24768d33ed7602a0cc9ecccbd4e497fc2993c5460e4e0d4b52572d5a1dfd4ce0cd72b7a8e2d95e1dd256b2c9cee51fdc46521675416de8fc7f655998c6e'
